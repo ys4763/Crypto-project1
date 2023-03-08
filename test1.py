2da05b4d8508a5f3e68f607714aacc3a63e077f3
ys4763 <ys4763@nyu.edu>
2023-03-08 16:47:52 -0500

    in order to switch branch lol

--- test1.py
+++ test1.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 import random
 
-def test1():
+def test1(ct):
+
+	l = len(ct)
+	prob = (l-600) / 600
+	
+	
+	
 	return random.randint(1,5)
 	
 if __name__ == "__main__":
 	
 	ct = input("Enter the ciphertext: ")
-	choice = test(ct)
+	choice = test1(ct)
 	print("My plaintext guess is: " + choice)
